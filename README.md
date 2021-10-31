d1f88152818ebf91478f6d5cce9ba6eb472f5af3
Jay Hechter <hechtej@wwu.edu>
2021-10-31 16:23:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Presently simply shows an estimated depth stream from the OAK-D camera.
+
+Look into the depthai and opencv APIs for programming info.
+
+Can simply be run with `python3 main.py`.  If no camera is connected, the program will crash.
+
+Other, more complicated sample programs with more fancy features are available.  We can look to those for inspiration if we wish.  https://github.com/luxonis/depthai-experiments
---
+++ main.py
@@ -0,0 +1,90 @@
+import cv2
+import depthai as dai
+import numpy as np
+
+def getFrame(queue):
+	# Get frame from queue
+	frame = queue.get()
+	# Convert frame to OpenCV format
+	return frame.getCvFrame()
+
+def getMonoCamera(pipeline, isLeft):
+	mono = pipeline.createMonoCamera()
+	
+	mono.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+
+	if isLeft:
+		mono.setBoardSocket(dai.CameraBoardSocket.LEFT)
+	else:
+		mono.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+	return mono
+
+def getStereoPair(pipeline, monoLeft, monoRight):
+	stereo = pipeline.createStereoDepth()
+	
+	# Turn on occlusion check (small performance hit, but output is less noisy)
+	stereo.setLeftRightCheck(True)
+	# Link mono cameras to the stereo pair
+	monoLeft.out.link(stereo.left)
+	monoRight.out.link(stereo.right)
+
+	return stereo
+
+if __name__ == '__main__':
+	pipeline = dai.Pipeline()
+	
+	# Get side cameras
+	monoLeft = getMonoCamera(pipeline, isLeft = True)
+	monoRight = getMonoCamera(pipeline, isLeft = False)
+	
+	stereo = getStereoPair(pipeline, monoLeft, monoRight)
+	
+	# Define output depth map
+	xOutDepth = pipeline.createXLinkOut()
+	xOutDepth.setStreamName("depth")
+	
+	xOutDisp = pipeline.createXLinkOut()
+	xOutDisp.setStreamName("disparity")
+
+	xOutRectifiedLeft = pipeline.createXLinkOut()
+	xOutRectifiedLeft.setStreamName("rectifiedLeft")
+
+	xOutRectifiedRight = pipeline.createXLinkOut()
+	xOutRectifiedRight.setStreamName("rectifiedRight")
+	
+	stereo.disparity.link(xOutDisp.input)
+	
+	stereo.rectifiedLeft.link(xOutRectifiedLeft.input)
+	stereo.rectifiedRight.link(xOutRectifiedRight.input)
+	
+	# Connect device
+	with dai.Device(pipeline) as device:
+		disparityQueue = device.getOutputQueue(name="disparity", maxSize=1, blocking=False)
+		rectifiedLeftQueue = device.getOutputQueue(name="rectifiedLeft", maxSize=1, blocking=False)
+		rectifiedRightQueue = device.getOutputQueue(name="rectifiedRight", maxSize=1, blocking=False)
+		
+		# map disparity from 0 to 255
+		disparityMultiplier = 255 / stereo.getMaxDisparity()
+
+		cv2.namedWindow("Stereo Pair")
+		
+		while True:
+			disparity = getFrame(disparityQueue)
+			
+			disparity = (disparity * disparityMultiplier).astype(np.uint8)
+			
+			leftFrame = getFrame(rectifiedLeftQueue)
+			rightFrame = getFrame(rectifiedRightQueue)
+			
+			# Show stereo view by averaging left and right cameras
+			imOut = np.uint8(leftFrame/2 + rightFrame/2)
+
+
+			cv2.imshow("Stereo Pair", imOut)
+			cv2.imshow("Disparity", disparity)
+
+			# Check for keyboard input
+			key = cv2.waitKey(1)
+			if key == ord('q'):
+				# Quit when q is pressed
+				break
