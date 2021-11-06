9b2b9399dcc703b3164ca1a9382b0c17818150ec
Jay Hechter <hechtej@wwu.edu>
2021-11-05 21:45:43 -0700

    Rudimentary UI, improved depth estimation

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# IndoorNavigation - [InsertProjectNameHere]
-##Overview
-Presently simply shows an estimated depth stream from the OAK-D camera.
+# IndoorNavigation
 
-Look into the depthai and opencv APIs for programming info.
+## Overview
+Shows an estimated depth stream from the OAK-D camera.  Based on how much this deviates from a pre-determined average depth, a danger value is calculated which, if it exceeds a certain threshold, may warrant an emergency stop.
 
 Can simply be run with `python3 main.py`.  If no camera is connected, the program will crash.
 
-Other, more complicated sample programs with more fancy features are available.  We can look to those for inspiration if we wish.  https://github.com/luxonis/depthai-experiments
-
-##SETUP
+## Setup
 ```
 $ sudo apt install python3
 $ sudo apt install virtualenv
@@ -21,3 +18,8 @@ $ python3 main.py
  # to deactivate virtualenv #
 $ deactivate 
 ```
+
+## Programming
+Look into the depthai and opencv APIs for programming info.
+
+Other, more complicated sample programs with more fancy features are available.  We can look to those for inspiration if we wish.  https://github.com/luxonis/depthai-experiments
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import cv2
 import depthai as dai
 import numpy as np
+import time
 
 MEASURED_AVERAGE = 255/771.665 #(max_dist-min_dist)/2+min_dist then converted to 0->255 range
-WARNING_THRESHOLD = 20
-DANGER_THRESHOLD = 50
+
+ESTIMATED_SAFE_VALUE = 140 # Camera pointed about 30 degrees down 14 inches from the ground reads this pretty consistently
+WARNING_THRESHOLD = 5
+DANGER_THRESHOLD =  10
 
 CAM_WIDTH = 640
 CAM_HEIGHT = 400
+
+WINDOW = "Indoor Nav"
 
 def getFrame(queue):
 	# Get frame from queue
@@ -37,6 +42,14 @@ def getStereoPair(pipeline, monoLeft, monoRight):
 
 	return stereo
 
+# UI helper functions to workaround lambda arguments
+def makeSlider(name, window, a_min, a_max):
+	cv2.createTrackbar(name, window, a_min, a_max, (lambda x: x))
+	cv2.setTrackbarPos(name, window, int((a_min+a_max)/2))
+
+def setSlider(name, window, val):
+	cv2.setTrackbarPos(name,window,val)
+
 if __name__ == '__main__':
 	pipeline = dai.Pipeline()
 	
@@ -54,10 +67,9 @@ if __name__ == '__main__':
 	# Connect device
 	with dai.Device(pipeline) as device:
 		disparityQueue = device.getOutputQueue(name="disparity", maxSize=1, blocking=False)
-		depthQueue     = device.getOutputQueue(name="depth", maxSize=1, blocking=False)
 		
 		# map disparity from 0 to 255
-		disparityMultiplier = 255 / stereo.getMaxDisparity()
+		disparityMultiplier = 255 / stereo.initialConfig.getMaxDisparity()
 		
 		'''
 			Disparity: Double array of uint8
@@ -68,35 +80,53 @@ if __name__ == '__main__':
 				[X][X] is bottom right
 
 		'''
+
+		cv2.namedWindow(WINDOW)
+
+		makeSlider("Danger", WINDOW, 0, DANGER_THRESHOLD)
+
+		skipFrame = False
+
 		while True:
-			disparity = getFrame(disparityQueue)
-			disparity = (disparity * disparityMultiplier).astype(np.uint8)
+			# If we spend every iteration of the loop just processing the frames, we get to a point
+			# where keyboard inputs are HUGELY delayed (presumably some opencv event loop gets 
+			# overloaded?).  quick hacky fix is to spend every other iteration not doing anything 
+			# other than user input
+			skipFrame = not skipFrame
+			if not skipFrame:
+				disparity = getFrame(disparityQueue)
+				disparity = (disparity * disparityMultiplier).astype(np.uint8)
 
-			# Loop over every pixel to get average
-			distance_sum = 0
-			for col in range(0, CAM_WIDTH):
-				for row in range(0, CAM_HEIGHT):
-					distance_sum = distance_sum + disparity[row][col]
+				# Loop over every pixel to get average
+				distance_sum = 0
+				sample_count = 0
+				for col in range(0, CAM_WIDTH):
+					for row in range(0, CAM_HEIGHT):
+						# disparity reading of 0 means the true value is unknown.
+						# there's probably some clever way of patching the gaps by interpolating
+						# nearby valid readings, but for now we'll just ignore it.
+						# this can cause some issues when things are too close to the camera for
+						# proper readings, but ideally we'll catch the danger before it's that close
+						if disparity[row][col] != 0:
+							sample_count = sample_count + 1
+							distance_sum = distance_sum + disparity[row][col]
 			
-			distance_avg = distance_sum / (CAM_HEIGHT*CAM_WIDTH)
+				distance_avg = distance_sum / sample_count
 
-			#abs() allows for inclines & declines
-			ratio_avg = abs(distance_avg/MEASURED_AVERAGE)
+				# danger increases if above or below the calibrated value, detecting both
+				# obstacles and drops.  we cap it at DANGER_THRESHOLD for display.
+				# an emergency stop should be triggered if we exceed WARNING_THRESHOLD
+				danger = int(min(abs(distance_avg - ESTIMATED_SAFE_VALUE), DANGER_THRESHOLD))
 
-			#Detect if current dist_avg is off from the pre-measured average
-			if distance_avg > DANGER_THRESHOLD:
-				#ALERT_DANGER
-				print("DANGER")
+				setSlider("Danger", WINDOW, danger)
 
-			elif distance_avg > WARNING_THRESHOLD:
-				#ALERT_WARNING
-				print("WARNING")
-
-
-			cv2.imshow("Disparity", disparity)
+				cv2.imshow(WINDOW, disparity)
 
 			# Check for keyboard input
 			key = cv2.waitKey(1)
 			if key == ord('q'):
 				# Quit when q is pressed
 				break
+
+
+		cv2.destroyAllWindows()
