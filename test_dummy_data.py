70bdcbf1b6897f09ef20aae930b9e626919c5383
Timothy-Poehlman <timothypoehlman@gmail.com>
2021-11-28 19:36:40 -0800

    updated test_dummy_data to accept new tests

--- test_dummy_data.py
+++ test_dummy_data.py
@@ -2,9 +2,21 @@ import csv
 import main import analyze_frame
 
 if __name__ == '__main__':
-    # Test wall
+    test("basket")
+    test("drop")
+    test("empty")
+    test("mug")
+    test("table")
+    test("wall")
+
+
+def test(test_name):
+    print("Testing for "+test_name+"---------------")
+    if test_name == "empty":
+        print("--THERE SHOULD BE NO WARNINGS OR DANGER HERE--")
+    # Test test_name
     for index in range(0,60): 
-        dummy_frame_csv = open("/testing/test_wall/dummy_data_wall_"+index+".csv") 
+        dummy_frame_csv = cv2.imread("testing/test_"+test_name+"/image"+str(index)+".png", cv2.IMREAD_UNCHANGED) 
         dummy_frame = []
         for row in dummy_frame_csv:
             dummy_frame.append(row)
@@ -13,15 +25,4 @@ if __name__ == '__main__':
             print("DANGER")
         else if danger > 5:
             print("WARNING")
-            
-    # Test empty
-    for index in range(0,60): 
-        dummy_frame_csv = open("/testing/test_wall/dummy_data_empty_"+index+".csv") 
-        dummy_frame = []
-        for row in dummy_frame_csv:
-            dummy_frame.append(row)
-        danger = int(min(abs(distance_avg - ESTIMATED_SAFE_VALUE), DANGER_THRESHOLD))
-        if danger > 10:
-            print("DANGER")
-        else if danger > 5:
-            print("WARNING")
+    print("Finished testing for "+test_name+"---------------")
