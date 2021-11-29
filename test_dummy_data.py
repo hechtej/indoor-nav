0097be165bcb4f9b592f8941b403814d564a81a1
Timothy-Poehlman <timothypoehlman@gmail.com>
2021-11-28 19:29:09 -0800

    finished modularization

--- test_dummy_data.py
+++ test_dummy_data.py
@@ -2,10 +2,26 @@ import csv
 import main import analyze_frame
 
 if __name__ == '__main__':
-    # Loop through 1 second of data
+    # Test wall
     for index in range(0,60): 
         dummy_frame_csv = open("/testing/test_wall/dummy_data_wall_"+index+".csv") 
         dummy_frame = []
         for row in dummy_frame_csv:
             dummy_frame.append(row)
         danger = analyze_frame(dummy_frame)
+        if danger > 10:
+            print("DANGER")
+        else if danger > 5:
+            print("WARNING")
+            
+    # Test empty
+    for index in range(0,60): 
+        dummy_frame_csv = open("/testing/test_wall/dummy_data_empty_"+index+".csv") 
+        dummy_frame = []
+        for row in dummy_frame_csv:
+            dummy_frame.append(row)
+        danger = analyze_frame(dummy_frame)
+        if danger > 10:
+            print("DANGER")
+        else if danger > 5:
+            print("WARNING")
