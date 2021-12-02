a07877716851bcb33be279fed951550e5f7adfe0
Timothy Poehlman <46605302+Timothy-Poehlman@users.noreply.github.com>
2021-12-01 17:00:44 -0800

    WIP: testing test_cases (#4)
    * changed how for loop works
    
    * removed import statement
    
    * changed for loop to run for the number of files
    
    * removed bad files
    
    * Now analyzes frame in comparison to safe reference frame
    
    * Cleaned and updated test_dummpy_data.py
    
    * Made test output less verbose
    
    Co-authored-by: Jay Hechter <hechtej@wwu.edu>

--- test_dummy_data.py
+++ test_dummy_data.py
@@ -1,16 +1,6 @@
-import csv
 import cv2
 import main 
-import analyze_frame
-
-MEASURED_AVERAGE = 255/771.665 #(max_dist-min_dist)/2+min_dist then converted to 0->255 range
-
-ESTIMATED_SAFE_VALUE = 140 # Camera pointed about 30 degrees down 14 inches from the ground reads this pretty consistently
-WARNING_THRESHOLD = 5
-DANGER_THRESHOLD =  10
-
-CAM_WIDTH = 640
-CAM_HEIGHT = 400
+import os
 
 WINDOW = "Indoor Nav"
 
@@ -18,18 +8,33 @@ def test(test_name):
     print("Testing for "+test_name+"---------------")
     if test_name == "empty":
         print("--THERE SHOULD BE NO WARNINGS OR DANGER HERE--")
+    path, dirs, files = next(os.walk("testing/test_"+test_name))
+    file_count = len(files)
+
+    reference = main.get_reference()
+
+    danger_count = 0
+    warning_count = 0
+    safe_count = 0
+
     # Test test_name
-    for index in range(0,60): 
-        dummy_frame_csv = cv2.imread("testing/test_"+test_name+"/image"+str(index)+".png", cv2.IMREAD_UNCHANGED) 
-        dummy_frame = []
-        for row in dummy_frame_csv:
-            dummy_frame.append(row)
-        danger = int(min(abs(distance_avg - ESTIMATED_SAFE_VALUE), DANGER_THRESHOLD))
-        if danger > 10:
-            print("DANGER")
-        elif danger > 5:
-            print("WARNING")
-    print("Finished testing for "+test_name+"---------------")
+    for index in range(0,file_count): 
+        dummy_frame = cv2.imread("testing/test_"+test_name+"/image"+str(index)+".png", cv2.IMREAD_UNCHANGED) 
+
+        danger, result = main.analyze_frame(dummy_frame, reference)
+
+        if danger >= 7:
+            danger_count = danger_count + 1
+        elif danger >= 3:
+            warning_count = warning_count + 1
+        else:
+            safe_count = safe_count + 1
+
+    print("Safe frames:      "+str(safe_count))
+    print("Warning frames:   "+str(warning_count))
+    print("Dangerous frames: "+str(danger_count))
+
+    print("Finished testing for "+test_name+"---------------\n")
 
 if __name__ == '__main__':
     test("basket")
